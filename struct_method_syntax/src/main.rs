280dfa38eebf1bc243c2b0798d405710c95b1200
Ricardo Havill <rhavill2002@yahoo.com>
2019-07-07 10:42:47 -0400

    Added struct method examples.

---
+++ struct_method_syntax/src/main.rs
@@ -0,0 +1,54 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
+// Each struct is allowed to have multiple impl blocks. In this case, it 
+// probably doesn't make sense to do so.
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+// }
+// impl Rectangle {
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
+// impl Rectangle {
+//     fn square(size: u32) -> Rectangle {
+//         Rectangle { width: size, height: size }
+//     }
+// }
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+    println!("sq is {:?}", sq);
+
+}
