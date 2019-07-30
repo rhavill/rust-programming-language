cf6c333ca212f39e21b923df0239a34b0963e33f
Ricardo Havill <rhavill2002@yahoo.com>
2019-07-29 22:38:26 -0400

    Added some notes/comments.

--- 9_error_handling/panic_or_no_panic/src/main.rs
+++ 9_error_handling/panic_or_no_panic/src/main.rs
@@ -1,3 +1,15 @@
+/*
+Rust’s error handling features are designed to help you write more robust code. 
+The panic! macro signals that your program is in a state it can’t handle and 
+lets you tell the process to stop instead of trying to proceed with invalid or 
+incorrect values. The Result enum uses Rust’s type system to indicate that 
+operations might fail in a way that your code could recover from. You can use 
+Result to tell code that calls your code that it needs to handle potential 
+success or failure as well. Using panic! and Result in the appropriate 
+situations will make your code more reliable in the face of inevitable 
+problems.
+*/
+
 fn main() {
     validation();
 }
