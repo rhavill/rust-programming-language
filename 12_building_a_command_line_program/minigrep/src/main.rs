e7c08587a8a1ded3646e65ceeabb465c9859def3
Ricardo Havill <rhavill2002@yahoo.com>
2019-07-31 23:13:17 -0400

    Moved run function and Config struct into library crate.

--- 12_building_a_command_line_program/minigrep/src/main.rs
+++ 12_building_a_command_line_program/minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,42 +13,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    /*
-    We’ve declared the run function’s success type as () in the signature, which 
-    means we need to wrap the unit type value in the Ok value. This Ok(()) 
-    syntax might look a bit strange at first, but using () like this is the 
-    idiomatic way to indicate that we’re calling run for its side effects only; 
-    it doesn’t return a value we need.
-    */
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
