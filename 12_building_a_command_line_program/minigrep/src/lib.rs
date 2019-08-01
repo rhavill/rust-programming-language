5f4e897d84ead6576480b2c0fe03e1e5fda95a2a
Ricardo Havill <rhavill2002@yahoo.com>
2019-07-31 23:38:39 -0400

    Wrote a failing test and an empty function.

--- 12_building_a_command_line_program/minigrep/src/lib.rs
+++ 12_building_a_command_line_program/minigrep/src/lib.rs
@@ -22,8 +22,6 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
-
     /*
     We’ve declared the run function’s success type as () in the signature, which 
     means we need to wrap the unit type value in the Ok value. This Ok(()) 
@@ -32,4 +30,27 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     it doesn’t return a value we need.
     */
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
 }
