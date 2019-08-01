85fd696bc53e53f2f79b9effdb70d09376a2b0e9
Ricardo Havill <rhavill2002@yahoo.com>
2019-08-01 00:19:30 -0400

    Added case insensitive search function.

--- 12_building_a_command_line_program/minigrep/src/lib.rs
+++ 12_building_a_command_line_program/minigrep/src/lib.rs
@@ -25,7 +25,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     for line in search(&config.query, &contents) {
         println!("{}", line);
     }
-    
+
     /*
     We’ve declared the run function’s success type as () in the signature, which 
     means we need to wrap the unit type value in the Ok value. This Ok(()) 
@@ -46,6 +46,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -63,4 +76,35 @@ Pick three.";
             search(query, contents)
         );
     }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+
 }
