2628d04b4047fd7b2a480f57f478188930b48ed6
Ricardo Havill <rhavill2002@yahoo.com>
2019-08-03 08:27:49 -0400

    Added documentation comments to the add_one crate.

--- 14_more_about_cargo_and_crates/add/add_one/src/lib.rs
+++ 14_more_about_cargo_and_crates/add/add_one/src/lib.rs
@@ -1,7 +1,38 @@
+//! # add_one
+//!
+//! `add_one` is a collection of utilities that only contains one simple 
+//! function: add_one.
+
 #[allow(unused_imports)]
 use rand;
 
+/*
+We can generate the HTML documentation from this documentation comment by 
+running cargo doc. This command runs the rustdoc tool distributed with Rust and 
+puts the generated HTML documentation in the target/doc directory.
 
+For convenience, running cargo doc --open will build the HTML for your current 
+crate’s documentation (as well as the documentation for all of your crate’s 
+dependencies) and open the result in a web browser. 
+
+Another style of doc comment, //!, adds documentation to the item that contains 
+the comments rather than adding documentation to the items following the 
+comments. We typically use these doc comments inside the crate root file 
+(src/lib.rs by convention) or inside a module to document the crate or the 
+module as a whole.
+*/
+
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = add_one::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
 pub fn add_one(x: i32) -> i32 {
     x + 1
 }
