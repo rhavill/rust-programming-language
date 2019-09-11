44bac7df3127d4a5035c2b1d5ddb8a68c8721184
Ricardo Havill <rhavill2002@yahoo.com>
2019-09-10 23:38:09 -0400

    Added some memory leak examples.

---
+++ 15_smart_pointers/reference_cycles_can_leak_memory/src/main.rs
@@ -0,0 +1,134 @@
+/*
+We can see that Rust allows memory leaks by using Rc<T> and RefCell<T>: it’s 
+possible to create references where items refer to each other in a cycle. This 
+creates memory leaks because the reference count of each item in the cycle will 
+never reach 0, and the values will never be dropped.
+*/
+use std::rc::{Rc, Weak};
+use std::cell::RefCell;
+use crate::List::{Cons, Nil};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+fn main () {
+    creating_reference_cycle();
+    tree_structure();
+    vizualize_strong_and_weak_count();
+}
+
+fn creating_reference_cycle() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
+}
+
+/*
+You can also create a weak reference to the value within an Rc<T> instance by 
+calling Rc::downgrade and passing a reference to the Rc<T>. When you call 
+Rc::downgrade, you get a smart pointer of type Weak<T>. Instead of increasing 
+the strong_count in the Rc<T> instance by 1, calling Rc::downgrade increases the 
+weak_count by 1. The Rc<T> type uses weak_count to keep track of how many 
+Weak<T> references exist, similar to strong_count. The difference is the 
+weak_count doesn’t need to be 0 for the Rc<T> instance to be cleaned up.
+*/
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+fn tree_structure() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());    
+}
+
+fn vizualize_strong_and_weak_count() {
+let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );    
+}
