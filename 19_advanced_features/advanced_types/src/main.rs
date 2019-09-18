62cc5942fe5e882548805a2b090a5b780dc29b84
Ricardo Havill <rhavill2002@yahoo.com>
2019-09-18 00:18:32 -0400

    Added another advanced type example.

--- 19_advanced_features/advanced_types/src/main.rs
+++ 19_advanced_features/advanced_types/src/main.rs
@@ -4,6 +4,7 @@
 fn main() {
     type_synonyms_with_type_aliases();
     never_type();
+    sized_trait();
 }
 
 fn type_synonyms_with_type_aliases() {
@@ -60,6 +61,76 @@ fn type_synonyms_with_type_aliases() {
 
         fn write_all(&mut self, buf: &[u8]) -> Result<()>;
         fn write_fmt(&mut self, fmt: Arguments) -> Result<()>;
+    }
+    */
+}
+
+fn never_type() {
+    /*
+    Rust has a special type named ! that’s known in type theory lingo as the 
+    empty type because it has no values. We prefer to call it the never type 
+    because it stands in the place of the return type when a function will never 
+    return. Here is an example:
+    fn bar() -> ! {
+        // --snip--
+    }
+    This code is read as “the function bar returns never.” Functions that return 
+    never are called diverging functions. We can’t create values of the type ! 
+    so bar can never possibly return.
+
+    continue has a ! value. That is, when Rust computes the type of guess, it 
+    looks at both match arms, the former with a value of u32 and the latter with 
+    a ! value. Because ! can never have a value, Rust decides that the type of 
+    guess is u32.
+
+    Expressions of type ! can be coerced into any other type. We’re allowed to 
+    end this match arm with continue because continue doesn’t return a value.
+
+    In this code, val has the type T and panic! has the type !, so the result of 
+    the overall match expression is T. This code works because panic! doesn’t 
+    produce a value; it ends the program. In the None case, we won’t be 
+    returning a value from unwrap, so this code is valid.
+    impl<T> Option<T> {
+        pub fn unwrap(self) -> T {
+            match self {
+                Some(val) => val,
+                None => panic!("called `Option::unwrap()` on a `None` value"),
+            }
+        }
+    }
+
+    One final expression that has the type ! is a loop:
+    print!("forever ");
+    loop {
+        print!("and ever ");
+    }
+    */
+}
+
+fn sized_trait() {
+    /*
+    To work with DSTs, Rust has a particular trait called the Sized trait to 
+    determine whether or not a type’s size is known at compile time. This trait 
+    is automatically implemented for everything whose size is known at compile 
+    time. In addition, Rust implicitly adds a bound on Sized to every generic 
+    function. That is, a generic function definition like this:
+
+    fn generic<T>(t: T) {
+        // --snip--
+    }
+
+    is actually treated as though we had written this:
+
+    fn generic<T: Sized>(t: T) {
+        // --snip--
+    }
+
+    By default, generic functions will work only on types that have a known size 
+    at compile time. However, you can use the following special syntax to relax 
+    this restriction:
+
+    fn generic<T: ?Sized>(t: &T) {
+        // --snip--
     }
     */
 }
